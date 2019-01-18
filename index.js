25116cf404f0ef85bc8184d4b9abaff42640732e
Daniel Wang <danielwpz@gmail.com>
2019-01-18 13:58:40 -0800

    Make this configurable

--- index.js
+++ index.js
@@ -7,11 +7,39 @@ const bodyparser = require('koa-bodyparser');
 const cors = require('@koa/cors');
 
 const middlewares = [];
+const middlewareNameMap = {
+  compress,
+  helmet,
+  logger,
+  bodyparser,
+  cors
+};
 
-middlewares.push(logger());
-middlewares.push(helmet());
-middlewares.push(cors());
-middlewares.push(bodyparser());
-middlewares.push(compress());
+function buildMiddlewares(mapping) {
+  if (!mapping) {
+    mapping = {};
+  }
 
-module.exports = compose(middlewares);
+  /**
+   * config and add default middlewares first
+   */
+  for (let name of Object.keys(middlewareNameMap)) {
+    if (middlewareNameMap.hasOwnProperty(name) && mapping[name] !== false) {
+      const middlewareConfig = mapping[name];
+      middlewares.push(middlewareNameMap[name](middlewareConfig));
+    }
+  }
+
+  /**
+   * add custom middlewares
+   */
+  for (const name of Object.keys(mapping)) {
+    if (!middlewareNameMap[name]) {
+      middlewares.push(mapping[name]);
+    }
+  }
+
+  return compose(middlewares);
+}
+
+module.exports = buildMiddlewares;
